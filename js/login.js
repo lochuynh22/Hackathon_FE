60d9bedf7d8b4d83bc4dcc82462d60aa09516406
lochuynh22 <huynhlatienloc123@gmail.com>
2025-04-24 08:47:13 +0700

    day2

---
+++ js/login.js
@@ -0,0 +1,14 @@
+let userList = [
+    {
+        id:1,
+        username:"user1",
+        password:"password123",
+        email:"user1@gmail.com"
+    },
+    {
+        id:2,
+        username:"user2",
+        password:"password456",
+        email:"user2@gmail.com"
+    },
+]
